9b35724f95964d2240fe5dfc35986daf887756cf
Lalit yadav <lalitydv51@gmail.com>
2022-09-25 01:31:15 +0530

    simaple api

---
+++ data.js
@@ -0,0 +1,6 @@
+ const data= [
+    {name:"lalit yadav",email:"lalitydv51@gamil.com",Contect:"9669469547", Gander:"male"},
+    {name:"prajwal yadav",email:"prajwal51@gamil.com",Contect:"9669469549", Gander:"male"},
+    {name:"pranjali yadav",email:"pranjali@gamil.com",Contect:"9669469548", Gander:"Female"},
+]
+module.exports=data
